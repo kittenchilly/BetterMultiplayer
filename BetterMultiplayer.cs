d733e645713e02429f8f8f2ea3c18384cad0a06c
saschahi <39934087+saschahi@users.noreply.github.com>
2021-11-30 12:12:15 +0100

    Include Herosmod Permission system
    helps public servers

--- BetterMultiplayer.cs
+++ BetterMultiplayer.cs
@@ -1,6 +1,3 @@
-using Microsoft.Xna.Framework;
-using Terraria;
-using Terraria.ID;
 using Terraria.ModLoader;
 
 
@@ -8,8 +5,44 @@ namespace BetterMultiplayer
 {
 	public class BetterMultiplayer : Mod
 	{
-		public BetterMultiplayer()
+        public Mod herosmod;
+        public static BetterMultiplayer instance;
+        public const string heropermission = "BetterMultiplayer";
+        public const string heropermissiondisplayname = "Better Multiplayer Config";
+        public bool hasPermission;
+
+        public BetterMultiplayer()
 		{
+
 		}
-	}
+
+        public override void Load()
+        {
+            instance = this;
+            herosmod = ModLoader.GetMod("HEROsMod");
+        }
+        public override void Unload()
+        {
+            instance = null;
+        }
+
+        public override void PostAddRecipes()
+        {
+            SetupHerosMod();
+        }
+
+        public void SetupHerosMod()
+        {
+            if (herosmod != null)
+            {
+                herosmod.Call(
+                    // Special string
+                    "AddPermission",
+                    // Permission Name
+                    heropermission,
+                    // Permission Display Name
+                    heropermissiondisplayname);
+            }
+        }
+    }
 }
