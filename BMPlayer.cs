aea5d227259eaff801b626030be8ef78ae8df479
kittenchilly <mario.pikmin.zelda@gmail.com>
2022-06-01 12:13:33 -0500

    Update 1.6.1

--- BMPlayer.cs
+++ BMPlayer.cs
@@ -7,42 +7,36 @@ namespace BetterMultiplayer
 {
 	public class BMPlayer : ModPlayer
 	{
-		public bool teamLoaded = false;
 		public string teamChosen = GetInstance<BMConfig>().TeamToJoin;
-		public int team;
+		public int team = 0;
 		public override void OnEnterWorld(Player player)
 		{
-			base.OnEnterWorld(player);
-			if (teamChosen != "None")
+			team = teamChosen switch
 			{
-				switch (teamChosen)
-				{
-					case "Red":
-						team = 1;
-						break;
-					case "Green":
-						team = 2;
-						break;
-					case "Blue":
-						team = 3;
-						break;
-					case "Yellow":
-						team = 4;
-						break;
-					case "Pink":
-						team = 5;
-						break;
-					default:
-						team = 1;
-						break;
-				}
+				"None" => 0,
+				"Red" => 1,
+				"Green" => 2,
+				"Blue" => 3,
+				"Yellow" => 4,
+				"Pink" => 5,
+				_ => 0,
+			};
+		}
 
-				if (player.team != team)
-				{
-					player.team = team;
-				}
+		public override void clientClone(ModPlayer clientClone)
+		{
+			clientClone.Player.team = team;
+		}
+
+		public override void SendClientChanges(ModPlayer clientPlayer)
+		{
+			if (clientPlayer.Player.team != team)
+			{
+				Main.player[Main.myPlayer].team = team;
+				NetMessage.SendData(MessageID.PlayerTeam, -1, -1, null, Main.myPlayer);
 			}
 		}
+
 
 		public override void UpdateDead() //no boss fight respawn
 		{
@@ -68,22 +62,6 @@ namespace BetterMultiplayer
 					return;
 				}
 				Player.respawnTimer++;
-			}
-		}
-
-		public override void SendClientChanges(ModPlayer localPlayer)
-		{
-			if (teamChosen != "None")
-			{
-				if (!teamLoaded)
-				{
-					if (Main.player[Main.myPlayer].team > 0)
-					{
-						NetMessage.SendData(MessageID.PlayerTeam, -1, -1, null, Main.myPlayer, 0f, 0f, 0f, 0, 0, 0);
-					}
-					teamLoaded = true;
-				}
-				base.SendClientChanges(localPlayer);
 			}
 		}
 	}
